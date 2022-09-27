dfbd2ad103db9221a012e3d4c2b170627a26d750
Olga_Migal <Tashaloriya@gmail.com>
2022-09-27 22:54:40 +0300

    Первый

---
+++ ConsoleVS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+
+class Kotik
+{
+    static public void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Kotik!");
+    }
+}
+
